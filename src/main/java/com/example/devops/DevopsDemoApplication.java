853b7d6210af4a7b6451e1162eb3b167e755d9dc
Shyam Bista <shyam.bista@foneloan.com>
2026-05-20 09:49:17 +0545

    fix: change annotation

--- src/main/java/com/example/devops/DevopsDemoApplication.java
+++ src/main/java/com/example/devops/DevopsDemoApplication.java
@@ -3,18 +3,18 @@ package com.example.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class DevopsDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsDemoApplication.class, args);
 	}
 
-	@GetMapping("")
+	@GetMapping("/")
 	public String greeting(){
 		return "<h2>Hello World</h2>";
 	}
