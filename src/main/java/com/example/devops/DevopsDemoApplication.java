d1e5887a105b131c1201d2e8ecd1f943fbf90943
Shyam Bista <shyam.bista@foneloan.com>
2026-05-20 09:38:14 +0545

    added a simple greeting message

--- src/main/java/com/example/devops/DevopsDemoApplication.java
+++ src/main/java/com/example/devops/DevopsDemoApplication.java
@@ -3,12 +3,20 @@ package com.example.devops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class DevopsDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+
+	@GetMapping("")
+	public String greeting(){
+		return "<h2>Hello World</h2>";
 	}
 
 }
